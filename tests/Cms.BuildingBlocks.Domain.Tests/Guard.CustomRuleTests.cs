35bb9055cc1ac31afe09b0b0a4ecfcf807ef6482
Cleudice Santos <cleudice.ms@gmail.com>
2026-01-21 21:46:26 -0300

    New tests

---
+++ tests/Cms.BuildingBlocks.Domain.Tests/Guard.CustomRuleTests.cs
@@ -0,0 +1,39 @@
+using Cms.BuildingBlocks.Domain.Errors;
+using Cms.BuildingBlocks.Domain.Tests.Abstractions.Dummies;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Cms.BuildingBlocks.Domain.Tests;
+
+public sealed class Guard_CustomRule_Tests
+{
+    [Fact]
+    public void Against_WithCustomRule_ShouldThrow_WhenRuleIsTrue()
+    {
+        var error = new DomainError("custom.rule.failed");
+
+        Action act = () =>
+            Guard.Against(
+                0,
+                v => v == 0,
+                error,
+                _: true);
+
+        DomainException exception = act.ShouldThrow<DomainException>();
+        exception.Error.ShouldBe(error);
+    }
+
+    [Fact]
+    public void Against_WithCustomRule_ShouldReturnValue_WhenRuleIsFalse()
+    {
+        var result = Guard.Against(
+            10,
+            v => v < 0,
+            TestDomainError.DummyError(),
+            _: true);
+
+        result.ShouldBe(10);
+    }
+}
