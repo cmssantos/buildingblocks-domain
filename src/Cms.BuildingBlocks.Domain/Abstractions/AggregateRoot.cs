ce084e8c9a48dbaeda19d45cbc6786f164e20b2a
Cleudice Santos <cleudice.ms@gmail.com>
2026-01-21 22:46:16 -0300

    Update

--- src/Cms.BuildingBlocks.Domain/Abstractions/AggregateRoot.cs
+++ src/Cms.BuildingBlocks.Domain/Abstractions/AggregateRoot.cs
@@ -1,4 +1,7 @@
 namespace Cms.BuildingBlocks.Domain.Abstractions;
 
 public abstract class AggregateRoot<TId> : Entity<TId>
-    where TId : IEntityId;
+    where TId : IEntityId
+{
+    public int Version { get; private set; }
+}
